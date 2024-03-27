7fa2fbf6038f1dea729ac00380e5f653ddee0329
Dan Havercroft <dan@wwebdesign.co.uk>
2024-03-27 14:44:27 +0000

    Added file for Python loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+# For - Iterative Loop
+# While - Conditional Loop
+
+students = ["Dan", "Kev", "Kenan", "Farida", "Blain", "Iqra"]
+
+for student in students:
+    print(f'Hello, {student}')
+
+my_number = 1
+
+while my_number < 10:
+    print("Less than 10")
+    my_number += 1%                                                                                                          
