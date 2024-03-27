abd43d604cd179b4b23ea4383778064ced2b5d36
Dan Havercroft <dan@wwebdesign.co.uk>
2024-03-27 14:57:17 +0000

    Added data types file

---
+++ data_types.py
@@ -0,0 +1,29 @@
+# Char - Single Character ''
+"a"
+# String - String of Chars ""
+"Dan"
+# Integer - Whole Number 1 4 10 123123123
+5
+# Float - Decimal Number (Floating Point Number)
+5.5
+# Boolean - True/False Yes/No 1/0
+True
+
+# [] - List or Array
+[1, 2, 3]
+[1, "Dan", True]
+
+my_list = [1, "Dan", True]
+print(my_list[3])
+
+# {} - Dictionary
+# Key - Value pair
+my_dict = {
+    "Name": "Dan",
+    "Age": 25,
+    "WorksAtUA92": True
+}
+
+print(my_dict["HomeAddress"])
+
+my_tuple = ("Dan", "Havercroft")
